ff3b44e645ca57cfd161ecddec75d2e72ccab82a
liuderchi <liuderchi@gmail.com>
2017-11-09 15:30:29 +0800

    handle PR webhook

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const { handleIssueWH } = require('./webhookHandler')
+const { handleIssueWH, handlePullRequestWH } = require('./webhookHandler')
 
 module.exports = (robot) => {
   robot.on(['issues.opened', 'issues.edited'], handleIssueWH)
+  robot.on(['pull_request.opened', 'pull_request.edited'], handlePullRequestWH)
 }
--- webhookHandler.js
+++ webhookHandler.js
@@ -1,3 +1,5 @@
+const R = require('ramda')
+
 const { getCommand, getNewContent } = require('./utils/template')
 
 const handleIssueWH = async context => {
@@ -20,6 +22,36 @@ const handleIssueWH = async context => {
   }
 }
 
+const handlePullRequestWH = async context => {
+  const {
+    body: prBody,
+    head: { repo: { owner: { login: owner }, name: repo } },
+    number,
+  } = context.payload.pull_request
+  const command = getCommand(prBody)
+
+  if (!command.action) return
+
+  try {
+    const param = {
+      body: getNewContent(command, prBody),
+      owner,
+      repo,
+      number,
+    }
+    if (command.action === 'DIALOG') {
+      await context.github.pullRequests.createReview(
+        R.assoc('event', 'COMMENT', param)
+      )
+    } else {
+      await context.github.pullRequests.update(param)
+    }
+  } catch (e) {
+    console.error(e)
+  }
+}
+
 module.exports = {
   handleIssueWH,
+  handlePullRequestWH,
 }
