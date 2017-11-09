df44b1e96a0edb6db370bad1190c904a765bd6ea
liuderchi <liuderchi@gmail.com>
2017-11-09 11:53:20 +0800

    fixup template

--- templates/dialog.js
+++ templates/dialog.js
@@ -1,13 +1,20 @@
 // markdown as a dialog
 
 const dialog = `
-<!-- this is result of command \`/template dialog\`-->
-### Pick one template and submit, bot will help you:
-  - [ ] \`/template issue\`
-  - [ ] \`/template pr\`
-  - [ ] \`/template feature\` to propose a new feature
+<!-- this is result of command \`/template\`-->
+### 🤖 Hello Human! Pick One Command:
 
-NOTE: you can add this template to your \`ISSUE_TEMPLATE.md\` or \`PULL_REQUEST_TEMPLATE.md\`
+  - [ ] \`/template issue\` to report a *[simple issue][issue]*
+  - [ ] \`/template pr\` to create a *[simple pull request][pr]*
+  - [ ] \`/template feature\` to propose a *[new feature][feature]*
+
+Great! Now :recycle:**Refresh This Page**. Bot will help you.
+
+> you can copy this template to your \`ISSUE_TEMPLATE.md\` or \`PULL_REQUEST_TEMPLATE.md\`
+
+[issue]: https://github.com/TalAter/open-source-templates
+[pr]: https://raw.githubusercontent.com/stevemao/github-issue-templates/master/simple/PULL_REQUEST_TEMPLATE.md
+[feature]: https://github.com/TalAter/open-source-templates
 `
 
 
